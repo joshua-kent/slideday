7004641f11cbfd57dba6bd9c2185762cb364d46b
Joshua Kent <hyperxbeat@gmail.com>
2020-07-21 22:26:39 +0100

    betterish documentation

--- src/gencache.sh
+++ src/gencache.sh
@@ -12,18 +12,21 @@ then
     echo "Creating $bckgCache and parent directories..."
     mkdir -p $cacheDir
     touch $bckgCache
-    bckgCacheLines=`cat $bckgCache | wc -l`
+    bckgCacheLines=0 # amount of lines in bckg.cache
 else
-    bckgCacheLines=`cat $bckgCache | wc -l`
+    bckgCacheLines=`cat $bckgCache | wc -l` # likewise
 
-    if [[ `sed -n 7p $bckgCache` == *`date +%x`* ]]
+    if [[ `sed -n 7p $bckgCache` == *`date +%x`* ]] # quit if bckg.cache is up to date
     then
         exit
     fi
 fi
 
 
-background=`python $currDir/genrand.py $currDay`
+background=`python $currDir/genrand.py $currDay` # generates random background based on seed (current day)
+
+
+# --- Retry if background is the same as last background ---
 
 attempts=1
 while [[ "BACKGROUND      $background" == `sed -n 5p $bckgCache` ]]
@@ -32,11 +35,11 @@ do
     background=$(python $currDir/genrand.py `expr $currDay \* $attempts`)
 done
 
+# --- Update bckg.cache file ---
+
 echo -e "This file is auto-generated daily by Slideday.
 Messing with it probably won't cause damage,
 but it is not recommended.\n" > $bckgCache
 echo "BACKGROUND      $background" >> $bckgCache
 echo "ATTEMPTS        $attempts" >> $bckgCache
 echo "DATE            `date +%x`" >> $bckgCache
-
-echo $background
--- src/genrand.py
+++ src/genrand.py
@@ -1,8 +1,8 @@
 #!/bin/env python
 
-# This script chooses a pseudo-random background,
-# taking a seed as an argument (this uses the amount
-# of days since the Unix epoch as the seed).
+# --- This script chooses a pseudo-random background,
+# --- taking a seed as an argument (this uses the amount
+# --- of days since the Unix epoch as the seed).
 
 
 import sys
@@ -10,15 +10,17 @@ import pathlib
 from time import time
 import random
 
-currentDir = pathlib.Path(__file__).absolute()
-backgroundsDir = pathlib.Path(currentDir).parent.parent.joinpath("backgrounds")
+currentFile = pathlib.Path(__file__).absolute() # current file's absolute path (slideday/src/genrand.py)
+backgroundsDir = pathlib.Path(currentFile).parent.parent.joinpath("backgrounds") # directory where backgrounds are located (slideday/backgrounds)
 
+# Create a list of backgrounds in backgrounds directory
 backgroundsList = []
 for picture in backgroundsDir.iterdir():
     backgroundsList.append(picture.name)
 
-seed = int(sys.argv[1])
+seed = int(sys.argv[1]) # set the seed to the first argument passed to the script (usually days since Unix epoch)
 
 random.seed(seed)
 chosenBackground = random.choice(backgroundsList)
-print(chosenBackground)
+
+print(chosenBackground) # prints the background to stdout
